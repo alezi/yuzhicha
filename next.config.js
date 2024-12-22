418a92950e04ede3705359e82bd72977d981e2cb
alezi <pangjianye@163.com>
2024-12-22 12:58:11 +0800

    更新api 网址

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true
+  },
+  // 修改API代理配置
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL 
+          ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+          : '/api/:path*'
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig 
